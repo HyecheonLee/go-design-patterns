a555bd574979443bf8dd477ce384be6740b4d88c
hyecheon <rainbow880616@gmail.com>
2020-06-05 23:15:06 +0900

    abstract_factory

---
+++ creational/abstract_factory/luxury_car.go
@@ -0,0 +1,14 @@
+package abstract_factory
+
+type LuxuryCar struct{}
+
+func (*LuxuryCar) GetDoors() int {
+	return 4
+}
+func (*LuxuryCar) GetWheels() int {
+	return 4
+}
+
+func (*LuxuryCar) GetSeats() int {
+	return 5
+}
